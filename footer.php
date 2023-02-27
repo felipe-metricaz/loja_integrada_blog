48e123b95628b00b30af5064ae39de90fdb9940c
miguel-henrique <miguelhenrique.as10@gmail.com>
2023-02-27 17:04:25 -0300

    initial adjustment in footer

--- footer.php
+++ footer.php
@@ -12,19 +12,9 @@
 ?>
 
 	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'loja_integrada_blog' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'loja_integrada_blog' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'loja_integrada_blog' ), 'loja_integrada_blog', '<a href="http://felipepeixoto.tecnologia.ws/">Felipe Peixoto</a>' );
-				?>
-		</div><!-- .site-info -->
+
+<h1>Footer</h1>
+
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
