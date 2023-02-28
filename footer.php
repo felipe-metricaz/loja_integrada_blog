6364d00b95963848bccb48e64371ab475461815b
miguel-henrique <miguelhenrique.as10@gmail.com>
2023-02-28 15:20:48 -0300

    footer initial layout

--- footer.php
+++ footer.php
@@ -13,7 +13,45 @@
 
 	<footer id="colophon" class="site-footer">
 
-<h1>Footer</h1>
+<div class="container">
+ <div class="row">
+  	<div class="col s4">
+		<h3>Teste</h3>
+		<img src="https://blog.lojaintegrada.com.br/wp-content/uploads/2022/03/Logo_Blog_Fundo-Branco-1-1-1024x124.png.webp" alt="">
+	</div>
+  <div class="col s2"><h3>Teste</h3></div>
+  <div class="col s2"><h3>Teste</h3></div>
+  <div class="col s2"><h3>Teste</h3></div>
+  <div class="col s2"><h3>Teste</h3></div>
+
+ </div>
+</div>
+
+<style>
+
+/* Footer styles */
+
+
+@media only screen and (max-width: 768px){
+	
+	footer .row{
+		display: grid;
+	}
+	
+	footer .row .s4{
+		display: grid;
+		width: 100% !important;
+		margin: 0px;
+		justify-items: center;
+	}
+
+	
+
+}
+
+/* End footer Styles */
+
+</style>
 
 	</footer><!-- #colophon -->
 </div><!-- #page -->
