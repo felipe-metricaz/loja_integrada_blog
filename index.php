5d7e4da637cd799cdc72be2b37bfba5a24da3956
miguel-henrique <miguelhenrique.as10@gmail.com>
2023-03-02 14:59:35 -0300

    body initial layout

--- index.php
+++ index.php
@@ -17,41 +17,53 @@ get_header();
 
 	<main id="primary" class="site-main">
 
-		<?php
-		if ( have_posts() ) :
+		<div class="container">
 
-			if ( is_home() && ! is_front_page() ) :
-				?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-				<?php
-			endif;
+			<div class="row">
+				<div class="col s6">
+					<article>
+						<div>
+							<a href="">
+								<div>
 
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
+								</div>
+							</a>
+						</div>
+						<div>
+							<div class="post-category">
+								<a href="">Categoria</a>
+							</div>
+							<div class="post-content">
+								<h3>Como montar um E-commerce</h3>
+								<div class="post-info">
+									<div class="autor">
+										<a href="">Ana</a>
+									</div>
+									<div class="data">
+										<a href="">Data</a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</article>
+				</div>
+				
 
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
+				<div class="col s4">
+					<article>
+						<h2>Teste</h2>
+					</article>
+					<article>
+						<h2>Teste</h2>
+					</article>
+				</div>
+			</div>
+			
+		</div>
 
-			endwhile;
 
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
 
 	</main><!-- #main -->
 
 <?php
-get_sidebar();
 get_footer();
