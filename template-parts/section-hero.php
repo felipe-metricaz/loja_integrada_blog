4f2ce1d0c05d8abeddaae55cc2e8f7e907665b4c
felipe152 <felipe.peixoto@metricaz.com>
2023-03-03 18:09:01 -0300

    começo do hero

--- template-parts/section-hero.php
+++ template-parts/section-hero.php
@@ -1,7 +1,21 @@
 
-<div class="container">
+<section class="container section-hero">
+    <h2 class="title-label">Mais recentes</h2>
+    <div class="divider"></div>
     <div class="row">
-
+        <div class="col s12 m6 no-padding-left">
+            <article>
+                <div class="gallery-thumbnail">
+                    <a href="">
+                        <img src="https://lojaintegrada.wpenginepowered.com/wp-content/uploads/2022/11/Ideias-criativas-de-nomes-para-loja-de-roupas.webp" alt="">
+                    </a>
+                    <div class="post-category">
+                        <a href="">Category</a>
+                    </div>
+                    <h2><a class="white" href="#">Como montar um ecommerce: Dicas para 2023</a></h2>
+                </div>
+            </article>
+        </div>
     </div>
 
-</div>
+</section>
