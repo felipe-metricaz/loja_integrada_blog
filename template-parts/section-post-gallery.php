3088958b522d5112dd813a18227eba0092f21290
miguel-henrique <miguelhenrique.as10@gmail.com>
2023-03-08 12:30:49 -0300

    updates

--- template-parts/section-post-gallery.php
+++ template-parts/section-post-gallery.php
@@ -153,5 +153,11 @@
            </article>
         </div>
     </div>
-   
+
+    <div class="row center-align">
+        <a href="">
+            <img src="https://lojaintegrada.wpenginepowered.com/wp-content/uploads/2022/03/image-1-1-1024x171.png" alt="">
+        </a>
+    </div>
+
 </div>
