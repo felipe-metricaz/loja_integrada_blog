395d0c21b222c4d2d919bb865dec93d1bc838197
miguel-henrique <miguelhenrique.as10@gmail.com>
2023-03-08 10:08:45 -0300

    updates

--- template-parts/section-post-gallery.php
+++ template-parts/section-post-gallery.php
@@ -2,32 +2,7 @@
     <h2 class="title-label">Queridinhos do momento</h2>
     <div class="divider"></div>
     <div class="row">
-        <div class="col s12 m4">
-           <article>
-                <div class="gallery-thumbnail">
-                    <a href="">
-                            <img src="https://lojaintegrada.wpenginepowered.com/wp-content/uploads/2022/11/Ideias-criativas-de-nomes-para-loja-de-roupas-360x180.webp" alt="">
-                    </a>
-                 
-                    <div class="over-legend">
-                        <div class="post-category">
-                            <a href="">Category</a>
-                        </div>
-                    </div>
-                    
-                </div>
-
-                <div class="gallery-post-info">
-                    <h4 >
-                        <a href="">Ideias criativas de nomes para loja de roupas</a>
-                    </h4>
-                    <div class="gallery-post-data">
-                       <a href="https://blog.lojaintegrada.com.br/ideias-criativas-de-nomes-para-loja-de-roupas/"><i class="tiny material-icons">access_time</i> 1 de dezembro de 2022</a>
-                    </div>
-                </div>
-           </article>
-        </div>
-        <div class="col s12 m4">
+        <div class="col s12 m4 no-padding-left">
            <article>
                 <div class="gallery-thumbnail">
                     <a href="">
@@ -53,6 +28,103 @@
            </article>
         </div>
         <div class="col s12 m4">
+           <article>
+                <div class="gallery-thumbnail">
+                    <a href="">
+                            <img src="https://lojaintegrada.wpenginepowered.com/wp-content/uploads/2022/11/Ideias-criativas-de-nomes-para-loja-de-roupas-360x180.webp" alt="">
+                    </a>
+                 
+                    <div class="over-legend">
+                        <div class="post-category">
+                            <a href="">Category</a>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="gallery-post-info">
+                    <h4 >
+                        <a href="">Ideias criativas de nomes para loja de roupas</a>
+                    </h4>
+                    <div class="gallery-post-data">
+                       <a href="https://blog.lojaintegrada.com.br/ideias-criativas-de-nomes-para-loja-de-roupas/"><i class="tiny material-icons">access_time</i> 1 de dezembro de 2022</a>
+                    </div>
+                </div>
+           </article>
+        </div>
+        <div class="col s12 m4 no-padding-right">
+           <article>
+                <div class="gallery-thumbnail">
+                    <a href="">
+                            <img src="https://lojaintegrada.wpenginepowered.com/wp-content/uploads/2022/11/Ideias-criativas-de-nomes-para-loja-de-roupas-360x180.webp" alt="">
+                    </a>
+                 
+                    <div class="over-legend">
+                        <div class="post-category">
+                            <a href="">Category</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="gallery-post-info">
+                    <h4 >
+                        <a href="">Ideias criativas de nomes para loja de roupas</a>
+                    </h4>
+                    <div class="gallery-post-data">
+                       <a href="https://blog.lojaintegrada.com.br/ideias-criativas-de-nomes-para-loja-de-roupas/"><i class="tiny material-icons">access_time</i> 1 de dezembro de 2022</a>
+                    </div>
+                </div>
+           </article>
+        </div>
+        <div class="col s12 m4 no-padding-left">
+           <article>
+                <div class="gallery-thumbnail">
+                    <a href="">
+                            <img src="https://lojaintegrada.wpenginepowered.com/wp-content/uploads/2022/11/Ideias-criativas-de-nomes-para-loja-de-roupas-360x180.webp" alt="">
+                    </a>
+                 
+                    <div class="over-legend">
+                        <div class="post-category">
+                            <a href="">Category</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="gallery-post-info">
+                    <h4 >
+                        <a href="">Ideias criativas de nomes para loja de roupas</a>
+                    </h4>
+                    <div class="gallery-post-data">
+                       <a href="https://blog.lojaintegrada.com.br/ideias-criativas-de-nomes-para-loja-de-roupas/"><i class="tiny material-icons">access_time</i> 1 de dezembro de 2022</a>
+                    </div>
+                </div>
+           </article>
+        </div>
+        <div class="col s12 m4">
+           <article>
+                <div class="gallery-thumbnail">
+                    <a href="">
+                            <img src="https://lojaintegrada.wpenginepowered.com/wp-content/uploads/2022/11/Ideias-criativas-de-nomes-para-loja-de-roupas-360x180.webp" alt="">
+                    </a>
+                 
+                    <div class="over-legend">
+                        <div class="post-category">
+                            <a href="">Category</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="gallery-post-info">
+                    <h4 >
+                        <a href="">Ideias criativas de nomes para loja de roupas</a>
+                    </h4>
+                    <div class="gallery-post-data">
+                       <a href="https://blog.lojaintegrada.com.br/ideias-criativas-de-nomes-para-loja-de-roupas/"><i class="tiny material-icons">access_time</i> 1 de dezembro de 2022</a>
+                    </div>
+                </div>
+           </article>
+        </div>
+        <div class="col s12 m4 no-padding-right">
            <article>
                 <div class="gallery-thumbnail">
                     <a href="">
